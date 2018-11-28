4e2f1d1884daed4f2005dbc56850f32f9e9c4d95
be222bw <be222bw@student.lnu.se>
2018-11-28 20:14:28 +0100

    Simplifying soft 17 hit strategy.

--- BlackJack/model/rules/Soft17HitStrategy.java
+++ BlackJack/model/rules/Soft17HitStrategy.java
@@ -8,20 +8,12 @@ public class Soft17HitStrategy implements IHitStrategy {
 	private final int g_hitLimit = 17;
 	@Override
 	public boolean DoHit(Player a_dealer) {
-		int score = 0;
 		boolean hasAce = false;
-		int cardScores[] = {
-		        2, 3, 4, 5, 6, 7, 8, 9, 10, 10 ,10 ,10, 11
-		    };
 		for (Card c : a_dealer.GetHand()) {
-			Card.Value v = c.GetValue();
-			hasAce |= v == Card.Value.Ace; // true | false == true, so if only one of the cards is ace, hasAce will remain true.
-			
-			if (v != Card.Value.Ace && v != Card.Value.Hidden) {
-				score += cardScores[v.ordinal()];
-			}
+			hasAce |= c.GetValue() == Card.Value.Ace; // true | false == true, so if only one of the cards is ace,
+			// hasAce will remain true.
 		}
-		return (hasAce && score == 6) || a_dealer.CalcScore() < g_hitLimit;
+		return (hasAce && a_dealer.CalcScore() == 17) || a_dealer.CalcScore() < g_hitLimit;
 	}
 
 }
