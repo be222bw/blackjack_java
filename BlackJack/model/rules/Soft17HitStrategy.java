2588ecf691f2f386f1dc8efb70084430f0dcc9d0
be222bw <be222bw@student.lnu.se>
2018-11-29 12:28:21 +0100

    Beautify code.

--- BlackJack/model/rules/Soft17HitStrategy.java
+++ BlackJack/model/rules/Soft17HitStrategy.java
@@ -4,16 +4,13 @@ import BlackJack.model.Card;
 import BlackJack.model.Player;
 
 public class Soft17HitStrategy implements IHitStrategy {
-
 	private final int g_hitLimit = 17;
 	@Override
 	public boolean DoHit(Player a_dealer) {
 		boolean hasAce = false;
 		for (Card c : a_dealer.GetHand()) {
-			hasAce |= c.GetValue() == Card.Value.Ace; // true | false == true, so if only one of the cards is ace,
-			// hasAce will remain true.
+			hasAce |= c.GetValue() == Card.Value.Ace; // true | false == true.
 		}
 		return (hasAce && a_dealer.CalcScore() == 17) || a_dealer.CalcScore() < g_hitLimit;
 	}
-
 }
